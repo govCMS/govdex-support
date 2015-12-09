b63b14be54092230b292a01129d54d9c032ca03e
paulkilla <paul.killer@gmail.com>
2015-12-09 14:31:03 +1100

    first commit

---
+++ templates/page--front.tpl.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="notFooter">
+  <header id="branding" class="row" role="banner">
+    <div class="container">
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="coa"><img width="288" height="64" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+      <img id="logo" width="90" height="90" alt="govdex" src="http://support.govdex.gov.au/wp-content/themes/supportDex/img/govdexLogo@2x.png">
+      <?php print render($page['header']); ?>
+    </div>
+  </header>
+
+  <div class="container">
+    <div class="row">
+      <?php print render($page['highlighted']); ?>
+    </div>
+
+    <div class="row">
+      <div id="corporateBlue" class="span8 offset2">
+          <div class="row">
+            <div class="span7">
+              <ul class="bigGrid">
+                <li>
+                  <a href="/administrators">
+                    <i class="fa fa-user"></i>
+                    <span>Administrators</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/members">
+                    <i class="fa fa-users"></i>
+                    <span>Members</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/contact-us">
+                    <i class="fa fa-envelope"></i>
+                    <span>Contact Us</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/notices/">
+                    <i class="fa fa-bullhorn"></i>
+                    <span>Notices</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/glossary">
+                    <i class="fa fa-book"></i>
+                    <span>Glossary</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/frequently-asked-questions">
+                    <i class="fa fa-question-circle"></i>
+                    <span>FAQs</span>
+                  </a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <div class="row">
+      <?php print render($page['footer_top']); ?>
+      <?php print render($page['footer_bottom']); ?>
+    </div>
+  </div>
+</div>
+
+<footer id="pageFooter" role="contentinfo">
+  <?php print render($page['bottom']); ?>
+</footer>
---
+++ templates/page.tpl.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
+
+<div id="notFooter">
+  <header id="branding" class="row" role="banner">
+    <div class="container">
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="coa"><img width="288" height="64" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+      <img id="logo" width="90" height="90" alt="govdex" src="http://support.govdex.gov.au/wp-content/themes/supportDex/img/govdexLogo@2x.png">
+      <?php print render($page['header']); ?>
+    </div>
+  </header>
+
+  <div class="container">
+    <?php
+    // Render the sidebar to see if there's anything in them.
+    $sidebar  = render($page['sidebar']);
+
+    $layout = $sidebar ? 'layout-sidebar' : 'layout-full';
+    ?>
+    <div class="row">
+      <?php print $breadcrumb; ?>
+
+      <?php print render($page['highlighted']); ?>
+    </div>
+
+    <div class="row">
+        <div class="span8 offset2" id="corporate">
+          <div class="container-inner">
+            <div class="<?php print $layout; ?>">
+
+              <div class="<?php print $layout; ?>__main main-content" role="main">
+
+                <a href="#skip-link" id="skip-content" class="element-invisible">Go to top of page</a>
+                <a id="main-content"></a>
+                <?php print render($title_prefix); ?>
+                <?php if ($title): ?>
+                  <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+                <?php endif; ?>
+                <?php print render($title_suffix); ?>
+                <?php print $messages; ?>
+                <?php print render($tabs); ?>
+                <?php print render($page['help']); ?>
+                <?php if ($action_links): ?>
+                  <ul class="action-links"><?php print render($action_links); ?></ul>
+                <?php endif; ?>
+                <?php print render($page['content']); ?>
+                <?php print $feed_icons; ?>
+              </div>
+
+              <?php if ($sidebar): ?>
+                <aside class="<?php print $layout; ?>__sidebar" role="complementary">
+                  <?php print $sidebar; ?>
+                </aside>
+              <?php endif; ?>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <div class="row">
+      <?php print render($page['footer_top']); ?>
+      <?php print render($page['footer_bottom']); ?>
+    </div>
+  </div>
+</div>
+
+<footer id="pageFooter" role="contentinfo">
+  <?php print render($page['bottom']); ?>
+</footer>
