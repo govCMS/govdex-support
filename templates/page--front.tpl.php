90c78eb94a9b1707ed60bed2ecdfea4a3402e699
Tim Hurley <tim@timhurley.net>
2016-02-09 20:21:33 +1100

    fixes and tweaks

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -11,9 +11,9 @@
 <div id="notFooter">
   <header id="branding" class="row" role="banner">
     <div class="container">
-      <a class="span12" href="/<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="coa">
-        <img id="coa" width="272" height="64" src="<?php echo drupal_get_path('theme',$GLOBALS['theme']) ?>/images/coas.svg" alt="Australian Government" class="header__logo-image" />
-        <img id="logo" width="78" height="90" alt="govdex" src="<?php echo drupal_get_path('theme',$GLOBALS['theme']) ?>/images/gds.svg">
+      <a class="span12" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="coa">
+        <img id="coa" width="272" height="64" src="/<?php echo drupal_get_path('theme',$GLOBALS['theme']) ?>/images/coas.svg" alt="Australian Government" class="header__logo-image" />
+        <img id="logo" width="78" height="90" alt="govdex" src="/<?php echo drupal_get_path('theme',$GLOBALS['theme']) ?>/images/gds.svg">
       </a>
       <?php print render($page['header']); ?>
     </div>
