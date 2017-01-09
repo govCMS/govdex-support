feedee9d849d46e39519b433fec0d5340a86ee0a
Ruwan <ruwan.lokusooriya@finance.gov.au>
2017-01-09 12:09:08 +1100

    GSCR-485 - Search field not working as expected

--- templates/block--search-api-page.tpl.php
+++ templates/block--search-api-page.tpl.php
@@ -1,16 +1,14 @@
 <form accept-charset="UTF-8" role="search" id="searchform" method="post" action="/search">
   <div>
     <label for="edit-keys-1" class="element-invisible screen-reader-text">Enter your keywords </label>
-    <input type="text" value="" name="edit-keys-1" id="edit-keys-1" placeholder="Search govdex support">
-    <button type="submit" id="searchsubmit" class="submit">
+    <input type="text" id="edit-keys-1" name="keys_1" value="" size="30" maxlength="128" class="form-text" placeholder="Search govdex support">
+    <?php print $variables['elements']['id']['#children']; ?>
+    <button type="submit" id="searchsubmit" class="submit" name="op" value="Search">
       <span class="fa fa-search"></span>
       <span class="screen-reader-text">Search</span>
     </button>
-
-    <input type="hidden" value="1" name="id">
-    <?php print $variables['elements']['base_1']['#children']; ?>
-    <?php print $variables['elements']['id']['#children']; ?>
     <?php print $variables['elements']['form_build_id']['#children']; ?>
+    <?php print $variables['elements']['form_token']['#children']; ?>
     <?php print $variables['elements']['form_id']['#children']; ?>
   </div>
 </form>
