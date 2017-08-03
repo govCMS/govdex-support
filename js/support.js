f888492ee3ae58a6aad49c29e97201ff16f39a0d
samChinACT <sam.chin@finance.gov.au>
2017-08-03 13:48:03 +1000

    update js for contact us form

--- js/support.js
+++ js/support.js
@@ -4,9 +4,22 @@
     attach: function (context, settings) {
       // Custom site scripts.
       $(document).ready(function(){
+        /*function test(){
+          console.log("function test");
+          document.getElementsByClassName("form-file").reset();
+        }*/
         if(window.location.href.indexOf("/contact-us") > -1) {
           $('#edit-submitted-attachments-upload', context).mousedown(function () {
+            var file = $('.form-file')[0].files[0];
+            if (!$('#file-clear').length) {
+              $('<button id=file-clear type=button size="22" style="border-radius: 6px; background-color: white; border: 1px solid darkgray; margin: 10px;  " onclick=jQuery(".form-file").replaceWith(jQuery(".form-file").val(\'\').clone(true));jQuery(this.remove());>Clear File</button>').insertBefore('#edit-submitted-attachments-upload-button');
+            }
+          });
+          $('#edit-submitted-attachments-upload-button', context).mousedown(function () {
+            var file = $('.form-file')[0].files[0];
+            if (file) {
               $(".webform-submit").prop('disabled', true);
+            }
           });
         $(document).ajaxComplete(function(){
           if ($('span').hasClass('file'))   {
