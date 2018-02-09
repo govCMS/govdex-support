79dd89a68029a92244fe543be4e7f83198106513
Joseph Zhao <joseph.zhao@xing.net.au>
2018-02-09 12:15:47 +1100

    Give header a span8

--- templates/notice/views-view--notices--page.tpl.php
+++ templates/notice/views-view--notices--page.tpl.php
@@ -34,7 +34,7 @@
   <?php endif; ?>
   <?php print render($title_suffix); ?>
   <?php if ($header): ?>
-    <div class="view-header span12">
+    <div class="view-header span8">
       <?php print $header; ?>
     </div>
   <?php endif; ?>
